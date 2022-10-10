872ea08cca703da6132b153a8867d83ae7249c68
lorddiamon <107939733+lorddiamon@users.noreply.github.com>
2022-10-10 16:32:25 -0600

    Update 5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-raise_exception = __import__('5-raise_exception').raise_exception
+# 5-raise_exception.py
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
 
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
